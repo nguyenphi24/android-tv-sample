33adf3c9cc2849f68c2450fa5f4589f1aab88742
tnphi <nguyenphi24@gmail.com>
2024-07-11 11:43:58 +0700

    init

---
+++ app/src/main/java/com/tpzone/android_tv_sample/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.tpzone.android_tv_sample
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+
+/**
+ * Created by tnphi on 7/11/2024.
+ */
+class MainActivity: FragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/tpzone/android_tv_sample/MainFragment.kt
@@ -0,0 +1,9 @@
+package com.tpzone.android_tv_sample
+
+import androidx.fragment.app.Fragment
+
+/**
+ * Created by tnphi on 7/11/2024.
+ */
+class MainFragment: Fragment() {
+}
